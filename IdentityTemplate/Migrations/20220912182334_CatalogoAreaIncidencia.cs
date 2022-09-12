6c9c0904b35d6a27b8e2eeff1ed0ef729fdd975f
@hector.jimenez <hector.jimenez@infotec.mx>
2022-09-12 13:24:16 -0500

    Creacion de la migracion del area de incidencia

---
+++ IdentityTemplate/Migrations/20220912182334_CatalogoAreaIncidencia.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IdentityTemplate.Migrations
+{
+    public partial class CatalogoAreaIncidencia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_cat_area_incidencia",
+                columns: table => new
+                {
+                    id_area_incidencia = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ln_descripcion_area = table.Column<string>(type: "varchar", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("id_n_area_incidencia", x => x.id_area_incidencia);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_cat_area_incidencia");
+        }
+    }
+}
